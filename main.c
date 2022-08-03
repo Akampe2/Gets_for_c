48b6c2d65e49db8db1891129b181999244b55d09
Akampe2 <37560111+Akampe2@users.noreply.github.com>
2022-08-03 14:09:50 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+typedef char *string;
+
+int get_int(string string)
+{
+    int n;
+    printf("%s", string.c_str());
+    scanf("%d", &n);
+
+    //printf("Height is: %i", n);
+    return n;
+}
+
+char * get_string(string string)
+{
+    char * m;
+
+    printf("%s",string.c_str());
+    scanf("%s", m);
+    printf("m in get_string is: %s\n",m);
+
+    return m;
+}
+
+int get_num()
+{
+    int num;
+
+    scanf("%d", &num);
+    printf("Num is %d\n", num);
+    //printf("Height is: %i", n);
+    return num;
+}
+
+
+int main() {
+
+    return 0;
+}
